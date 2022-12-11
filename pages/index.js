a0b65be00633e74ac55bad76fcfdf33ff7d18f90
Yagi91 <bryantimah@gmail.com>
2022-12-11 17:38:39 +0100

    Draw news headline data from BBC using news API through fetch

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,26 @@ import Sports from "../components/sections/sports";
 import Lifestyle from "../components/sections/lifestyle";
 import Videos from "../components/sections/videos";
 
-export default function Home() {
-  let cardInfo = [
-    {
-      title: "Craig Braton - Jan 2020",
-      headline: "They&apos;re back ! Kennedy Darling named to return to",
-      urlNum: "2",
-    },
-    {
-      title: "Craig Braton - Jan 2020",
-      headline: "Swiss authorities say Uber drivers should",
-      urlNum: "1",
-    },
-    {
-      title: "Craig Braton - Jan 2020",
-      headline: "Swiss authorities say Uber drivers should",
-      urlNum: "4",
-    },
-  ];
+let cardInfo = [
+  {
+    title: "Craig Braton - Jan 2020",
+    headline: "They&apos;re back ! Kennedy Darling named to return to",
+    urlNum: "5",
+  },
+  {
+    title: "Craig Braton - Jan 2020",
+    headline: "Swiss authorities say Uber drivers should",
+    urlNum: "1",
+  },
+  {
+    title: "Craig Braton - Jan 2020",
+    headline: "Swiss authorities say Uber drivers should",
+    urlNum: "1",
+  },
+];
+
+function Home({ card, headData }) {
+  console.log(headData);
   return (
     <>
       <section className="container mx-auto mb-4 lg:mb-8">
@@ -45,7 +47,7 @@ export default function Home() {
             </div>
           </div>
           <div className="hidden lg:grid gap-4 grid-cols-2">
-            {cardInfo.map((card, i) => {
+            {card.map((card, i) => {
               let bgClass =
                 `bg-[url('../public/images/` + card.urlNum + `.jpg')]`;
               let extraClass = i >= 2 ? "col-span-2" : "";
@@ -71,3 +73,43 @@ export default function Home() {
     </>
   );
 }
+
+// export async function getStaticProps() {
+//   //fetch data from API
+
+//   return {
+//     props: {
+//       card: cardInfo,
+//     },
+//     revalidate: 60,
+//   };
+// }
+
+export async function getServerSideProps(context) {
+  //fetch data from API
+  let pageSize = 4;
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&pageSize=" +
+      pageSize,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + process.env.NEXT_PUBLIC_NEWS_API,
+      },
+    }
+  );
+
+  const headData = await response.json();
+
+  console.log(headData);
+  console.log("hello you");
+
+  return {
+    props: {
+      card: cardInfo,
+      headData: headData,
+    },
+  };
+}
+
+export default Home;
