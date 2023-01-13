23e629b1da008fb8a07822bda2bedf97555fad52
Yagi91 <bryantimah@gmail.com>
2023-01-13 15:46:37 +0100

    Min block imgaes use Next/image , Add placeholder to minBlock Image, Add image url Parser to determine if the link is https/http

--- components/ui/minBlock.js
+++ components/ui/minBlock.js
@@ -1,10 +1,36 @@
+import Image from "next/image";
+
 function MinBlock({ headline, tagline, imgUrl }) {
+  function urlPasser(str) {
+    const regExp = /^http/gi;
+    if (regExp.test(str)) {
+      return str;
+    } else {
+      return "";
+    }
+  }
+
   return (
     <a href="#" className="grid grid-cols-4 gap-2">
       <div
-        className="col-span-1 bg-cover h-24"
-        style={{ backgroundImage: `url('${imgUrl}')` }}
-      ></div>
+        className="col-span-1 bg-cover h-24 relative"
+        // style={{ backgroundImage: `url('${imgUrl}')` }}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6 ))`,
+        }}
+      >
+        {urlPasser(imgUrl) && (
+          <Image
+            src={imgUrl}
+            alt=""
+            srcset=""
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        )}
+      </div>
       <div className="col-span-3">
         <p className="font-light text-xs">{tagline}</p>
         <h3 className="font-header text-xl my-2">{headline}</h3>
