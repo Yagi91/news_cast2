628d5c950b6ff93b4e184ca12967557f2500cda1
Yagi91 <bryantimah@gmail.com>
2023-02-09 04:33:58 +0100

    Remove link from nav tab

--- components/ui/navButton.js
+++ components/ui/navButton.js
@@ -1,10 +1,9 @@
 export default function NavButton({ name, extraClass }) {
   return (
-    <a
-      href="#"
+    <div
       className={`font-header text-white py-4 px-6 hover:bg-primary-500 hover:transition hover:duration-300 font-xl ${extraClass}`}
     >
       {name}
-    </a>
+    </div>
   );
 }
